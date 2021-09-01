d288a8bc0e7faf838b52b3303d1923d96f24eac8
rajnish1999 <rajnishtiwarihrt@gmail.com>
2021-09-01 17:32:32 +0530

    Added navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer'
 
 const App = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <Header />
+            <main className='py-3' >
+                <Container>
+                    <h1>Welcome to ProShop</h1>
+                </Container>
+            </main>
+            <Footer />
+        </>
     )
 }
 
